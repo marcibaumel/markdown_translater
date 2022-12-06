f8f63da757f0d115a4b046425f9757557bf192c1
Baumel Márton <marcibaumel@gmail.com>
2022-12-06 12:44:59 +0100

    input fix

--- source/src/App.tsx
+++ source/src/App.tsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import "bootstrap/dist/css/bootstrap.css";
 import './App.css';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Editor } from './components/Editor';
+import MDEditor from '@uiw/react-md-editor';
 
 function App() {
+  const [value, setValue] = useState("");
+
+const onChange = (e: React.FormEvent<HTMLInputElement>): void => {
+  console.log((e.target as HTMLInputElement).value);
+  setValue((e.target as HTMLInputElement).value);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 }
---
+++ source/src/components/Editor.tsx
@@ -0,0 +1,10 @@
+import MDEditor from '@uiw/react-md-editor'
+import React from 'react'
+
+export const Editor = () => {
+  return (
+    <div>
+        <MDEditor/>
+    </div>
+  )
+}
