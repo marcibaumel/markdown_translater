17dd428c9d8dfd22bae9b3921bbafef2c069de2a
Baumel Márton <marcibaumel@gmail.com>
2022-12-09 16:18:17 +0100

    animation init

--- source/src/App.tsx
+++ source/src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.css";
 import './App.css';
+import { motion } from 'framer-motion';
 import MDEditor from '@uiw/react-md-editor';
 
 function App() {
-  const [value, setValue] = useState("**Hello world!!!**");
+  const [value, setValue] = useState("## Hello");
 
   const onChange = (e: any): void => {
     console.log(e);
@@ -12,13 +13,18 @@ function App() {
   }
 
   return (
-    <div className="container mt-5" data-color-mode="light">
+    <motion.div
+      initial={{ opacity: 0, scale: 0.2 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.7 }}
+      className="container mt-5"
+      data-color-mode="light">
       <MDEditor
         height={500}
         value={value}
         onChange={onChange}
       />
-    </div>
+    </motion.div>
   );
 }
 
